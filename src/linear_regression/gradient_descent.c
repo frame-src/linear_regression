8a1627634cf27c5d1428777ccbe32f7b40ad9744
Francesco Messina <frmessin@student.42heilbronn.de>
2024-01-29 17:19:48 +0100

    feat: OLS

--- src/linear_regression/gradient_descent.c
+++ src/linear_regression/gradient_descent.c
@@ -7,8 +7,8 @@ float learning_rate()
     return (1);
 }
 // price;
-int price[13] = {3650,3800,4400,4450,5250,5350,5800,5990,5999,6200,6390,6390,6600};
-int km[13] = {240000, 139800,150500, 185530, 176000, 114800,166800,89000, 144500,84000,82029,63060,7400};
+int price[13] = {3650,3800,4400};//,4450,5250,5350,5800,5990,5999,6200,6390,6390,6600};
+int km[13] = {240000, 139800,150500};//, 185530, 176000, 114800,166800,89000, 144500,84000,82029,63060,7400};
 int len = 13;
 
 /*
@@ -35,13 +35,14 @@ float init_slope (int *x, int *y, float x_m, float y_m, int len)
     return (num / den);
 }
 
-float *initialize_prmtrs( void *x, void *y, int len, float *prms)
+float *ordinary_least_squares( void *x, void *y, int len, float *prms)
 {
     float y_m = mean(y, len);
     float x_m = mean(x, len);
 
     prms[0] = init_slope(x, y, x_m,y_m, len);
     prms[1] = init_intercept(x_m, y_m, prms[0]);
+    printf("%f, %f\n", prms[0], prms[1]);
     return prms;
 }
 
@@ -51,7 +52,7 @@ void gradient_descent()
     float *prms;
 
     prms = malloc ( sizeof(float) * 2);
-    prms = initialize_prmtrs(price, km, len, prms);
+    prms = ordinary_least_squares(km, price, len, prms);
     while( min != 0 || min > 0.009) {
         float *tmp = calculate_derivative_rss(km, price, len, prms);
         prms[0] = tmp[0];
