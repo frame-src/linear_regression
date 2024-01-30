652459c110a4f314fb6f276cf6e2e21e5a5f5841
Francesco Messina <iframe@live.it>
2024-01-30 20:26:33 +0100

    feat: i think it's better to use fprintf

--- src/output/window.c
+++ src/output/window.c
@@ -1,21 +1,26 @@
 #include <stdio.h> 
-
+#include <stdlib.h>
 #define IMG_WIDTH 256
 #define IMG_HEIGHT 256
 
 int print_image() 
 {
-
-    printf("P3\n%i %i\n255\n",IMG_WIDTH,IMG_HEIGHT);
+    FILE *file = fopen("./graphs/test.ppm", "w");
+    if (file == NULL) {
+        printf("Error opening the file.\n");
+        exit(1); // Indicates an error
+    }
+    fprintf(file,"P3\n%i %i\n255\n",IMG_WIDTH,IMG_HEIGHT);
 
     for (int j = 0; j < IMG_HEIGHT; ++j) {
         for (int i = 0; i < IMG_WIDTH; ++i) {
             int r = 248;
             int g = 124;
             int b = 86;
-            printf("%i %i %i\n", r, g, b);
+            fprintf(file,"%i %i %i\n", r, g, b);
         }
     }
+    fclose(file);
     return 1;
 }
 
