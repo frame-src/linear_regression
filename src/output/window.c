0eee8d8339d6d882aefb26bbdee0da8ff0441325
Francesco Messina <iframe@live.it>
2024-01-30 20:36:09 +0100

    to_do: insert points into the ppm

--- src/output/window.c
+++ src/output/window.c
@@ -1,23 +1,29 @@
 #include <stdio.h> 
 #include <stdlib.h>
+
 #define IMG_WIDTH 256
 #define IMG_HEIGHT 256
+#define BG {248,124,86}
+
+FILE* open_file( char *fpath)
+{
+    FILE *file = fopen(fpath, "w");
+    if (file == NULL) {
+        printf("Error opening the file.\n");
+        exit(1);
+    }
+    return file;
+}
 
 int print_image() 
 {
-    FILE *file = fopen("./graphs/test.ppm", "w");
-    if (file == NULL) {
-        printf("Error opening the file.\n");
-        exit(1); // Indicates an error
-    }
-    fprintf(file,"P3\n%i %i\n255\n",IMG_WIDTH,IMG_HEIGHT);
+    FILE *file = open_file("./graphs/test.ppm");
+    int rgb[] = BG;
 
+    fprintf(file,"P3\n%i %i\n255\n",IMG_WIDTH,IMG_HEIGHT);
     for (int j = 0; j < IMG_HEIGHT; ++j) {
         for (int i = 0; i < IMG_WIDTH; ++i) {
-            int r = 248;
-            int g = 124;
-            int b = 86;
-            fprintf(file,"%i %i %i\n", r, g, b);
+            fprintf(file,"%i %i %i\n", rgb[0], rgb[1], rgb[2]);
         }
     }
     fclose(file);
